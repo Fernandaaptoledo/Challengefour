6ec187b9a638081a56d992490ee1e4657cd9bf74
Fernandaaptoledo <fernanda-toledo2005@hotmail.com>
2023-02-13 08:06:57 -0300

    spring boot

---
+++ Spring-Boot-compass/src/main/java/com/br/challengefour/SpringBootcompass/SpringBootCompassApplication.java
@@ -0,0 +1,13 @@
+package com.br.challengefour.SpringBootcompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCompassApplication.class, args);
+	}
+
+}
